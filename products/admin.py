7f1e6b1311f306037353cf11229064dbb9ae5dbf
adamparker75 <adamparker75@sky.com>
2020-12-22 18:38:35 +0000

    Changes to products app

--- products/admin.py
+++ products/admin.py
@@ -3,9 +3,7 @@ from .models import Product, Category
 
 # Register your models here.
 
-
 class ProductAdmin(admin.ModelAdmin):
-    # A tuple that tells the admin which fields to display
     list_display = (
         'sku',
         'name',
@@ -15,16 +13,13 @@ class ProductAdmin(admin.ModelAdmin):
         'image',
     )
 
-    # Sort the productsby the sku
     ordering = ('sku',)
-
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
-
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
