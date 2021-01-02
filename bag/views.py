583c90de19e3c044db1b0ce5ec25fab5f38bfd26
adamparker75 <adamparker75@sky.com>
2021-01-02 11:41:44 +0000

    Finished product size logic

--- bag/views.py
+++ bag/views.py
@@ -16,14 +16,29 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     # Get the redirect from the form
     redirect_url = request.POST.get('redirect_url')
+    # If product size is in request
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     # Check to see if there is a bag variable in the sesssion
     # If not create one
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # Check if a product with sizes is being added
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
