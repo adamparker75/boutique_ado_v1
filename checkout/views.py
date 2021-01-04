7d95ac55b16f7d6a9d85ad33c0cb83b21ae6950a
adamparker75 <adamparker75@sky.com>
2021-01-04 19:49:34 +0000

    Installed crispy forms and created checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    # Get the bag from the session
+    bag = request.session.get('bag, {}')
+    # If there's nothing in the bag
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment.")
+        return redirect(reverse('products'))
+
+    # Create an instance of order form
+    order_form = OrderForm()
+    # Create the template
+    template = 'checkout/checkout.html'
+    # Create the context containing the form
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
