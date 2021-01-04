3d56509f8d73eac4a96af73604d4240cb1928f9f
adamparker75 <adamparker75@sky.com>
2021-01-04 19:15:58 +0000

    Added forms admin and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """ Allows us to add and edit line items in the admin
+    right from inside the order model.
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    """ These are all fields that will be calculated by the model methods.
+        We don't want anyone to be able to edit them
+    """
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    """Allows us to specify the order of the fields in the admin interface"""
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    """Restrict the columns that show up in the order
+     list to only a few key items.
+     """
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        # Dictionary of placeholders which shows in the form fields
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County',
+        }
+
+        # Setting the autofocus attribute on the full name field to true
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                # Add * to placeholder if required
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            # Set all placeholder attributes to vaules in dictionary above
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            # Add a CSS class
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            # Remove form fields labels
+            self.fields[field].label = False
