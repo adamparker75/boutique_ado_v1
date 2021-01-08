b234d36f1865b22bab046d77bf9b681dd6f3b219
adamparker75 <adamparker75@sky.com>
2021-01-08 18:46:36 +0000

    Completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -53,7 +53,12 @@ def checkout(request):
         order_form = OrderForm(form_data)
         # If the order form is valid we save the order
         if order_form.is_valid():
-            order = order_form.save()
+            # Commit=False prevents multiple save events being executed on the db
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             # Iterate through the bag items to create each line item
             for item_id, item_data in bag.items():
                 try:
