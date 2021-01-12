50e872757eb6b1fb7229e6bd9b561eda388fa485
adamparker75 <adamparker75@sky.com>
2021-01-12 18:18:41 +0000

    Created Product Form

---
+++ products/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+    # Define the model and fields we want to include
+    class Meta:
+        model = Product
+        # All of the fields from the product model
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        # Create a list of tuples of the friendly names associated with their category ID
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
